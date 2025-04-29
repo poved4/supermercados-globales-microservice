2d985e9ae8fcf0390367b869dc49348f742428b0
juan poveda <poved4@gmail.com>
2025-04-29 10:41:48 -0500

    added configurations module

---
+++ src/main/java/com/ucentral/microservice/configuration/AppConfig.java
@@ -0,0 +1,19 @@
+package com.ucentral.microservice.configuration;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+
+// import lombok.RequiredArgsConstructor;
+
+// @Configuration
+// @RequiredArgsConstructor
+public class AppConfig {
+
+  // @Bean
+  // public PasswordEncoder passwordEncoder() {
+  //   return new BCryptPasswordEncoder();
+  // }
+
+}
