e1236600b5556cfe204f2de8f7cb53b2c0abfa9e
juan poveda <poved4@gmail.com>
2025-05-28 11:05:39 -0500

    CORS fix

---
+++ src/main/java/com/ucentral/microservice/configuration/SecurityConfig.java
@@ -0,0 +1,35 @@
+// package com.ucentral.microservice.configuration;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
+
+// import lombok.RequiredArgsConstructor;
+
+// @Configuration
+// @EnableWebSecurity
+// @EnableMethodSecurity
+// @RequiredArgsConstructor
+// public class SecurityConfig {
+
+//   private final AuthorizationHeaderSecurityFilter authorizationHeaderSecurityFilter;
+
+//   @Bean
+//   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//     return http
+//       .csrf(csrf -> csrf.disable())
+//       .authorizeHttpRequests(auth -> auth
+//         .requestMatchers("/**").permitAll()
+//         .anyRequest().authenticated()
+//       )
+//       .addFilterBefore(
+//         authorizationHeaderSecurityFilter,
+//         org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class
+//       )
+//       .build();
+//   }
+
+// }
